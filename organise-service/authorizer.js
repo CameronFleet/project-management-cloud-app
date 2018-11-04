410b8da1c0f66774d83ee9f79b05962c3236708b
CameronFleet1998 <Cameron-Fleet@hotmail.com>
2018-11-04 20:58:54 +0000

    Added functions for authorization

---
+++ organise-service/authorizer.js
@@ -0,0 +1,39 @@
+
+
+var AWS = require('aws-sdk');
+var response = require('./lib/response');
+
+var ddb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.authorize = async (event, context) => {
+
+    var data = JSON.parse(event.body);
+
+    const params = {
+        TableName: "User",
+        Key: {
+            id: data.id
+        }
+    };
+
+    try {
+        const dbResponse = await ddb.get(params).promise();
+
+        if(dbResponse.Item) {
+
+            const userAccessLevel = dbResponse.Item.role;
+
+            if(userAccessLevel === data.accessLevel) {
+                return response.respondSuccess({allowed: true});
+            } else {
+                return response.respondFailure({allowed: false, error: "Not Authorized", accessLevel: userAccessLevel});
+            }
+
+        } else {
+            return response.respondFailure({allowed: false, error: "Username not found"});
+        }
+    } catch(e) {
+        return response.respondFailure({allowed: false, error: e.message});
+    }
+
+}
